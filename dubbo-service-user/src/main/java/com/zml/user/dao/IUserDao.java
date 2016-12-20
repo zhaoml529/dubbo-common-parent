481c592e7e3e935b4c275f56cfd9f06a3c626a95
zhaoml529 <zhaoml529@163.com>
2016-12-20 20:01:27 +0800

    修改接口

--- dubbo-service-user/src/main/java/com/zml/user/dao/IUserDao.java
+++ dubbo-service-user/src/main/java/com/zml/user/dao/IUserDao.java
@@ -3,9 +3,9 @@ package com.zml.user.dao;
 import com.zml.user.entity.User;
 
 public interface IUserDao {
-	public User getUserByName(String user_name) throws Exception;
+	public User getUserByName(String userName) throws Exception;
 	
 	public User getUserByStaffId(String staffId) throws Exception;
 
-	public User getUserById(Integer id) throws Exception;
+	public User getUserById(Long userId) throws Exception;
 }
