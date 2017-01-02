c1ff39a07f1ed83a1967243b7a511888f83b6f46
zhaoml529 <zhaoml529@163.com>
2017-01-02 16:57:46 +0800

    系统错误9009

--- dubbo-common/src/main/java/com/zml/common/exceptions/ServiceException.java
+++ dubbo-common/src/main/java/com/zml/common/exceptions/ServiceException.java
@@ -5,6 +5,12 @@ package com.zml.common.exceptions;
  * 定义异常时，需要先确定异常所属模块。例如：添加用户报错 可以定义为 [10010001] 前四位数为系统模块编号，后4位为错误代码(唯一)。
  * 
  * 用户服务异常 1001
+ * 公司服务接口 1002
+ * 部门服务接口 1003
+ * 岗位服务接口 1004
+ * 编制服务接口 1005
+ * 角色服务接口 1006
+ * 资源服务接口 1007
  * 
  * 系统异常 9009
  * 
@@ -21,36 +27,36 @@ public class ServiceException extends RuntimeException {
 	/**
 	 * 数据库操作,insert返回0
 	 */
-	public static final ServiceException DB_INSERT_RESULT_0 = new ServiceException(90040001, "数据库操作,insert返回0");
+	public static final ServiceException DB_INSERT_RESULT_0 = new ServiceException(90090001, "数据库操作,insert返回0");
 
 	/**
 	 * 数据库操作,update返回0
 	 */
-	public static final ServiceException DB_UPDATE_RESULT_0 = new ServiceException(90040002, "数据库操作,update返回0");
+	public static final ServiceException DB_UPDATE_RESULT_0 = new ServiceException(90090002, "数据库操作,update返回0");
 
 	/**
 	 * 数据库操作,selectOne返回null
 	 */
-	public static final ServiceException DB_SELECTONE_IS_NULL = new ServiceException(90040003, "数据库操作,selectOne返回null");
+	public static final ServiceException DB_SELECTONE_IS_NULL = new ServiceException(90090003, "数据库操作,selectOne返回null");
 
 	/**
 	 * 数据库操作,list返回null
 	 */
-	public static final ServiceException DB_LIST_IS_NULL = new ServiceException(90040004, "数据库操作,list返回null");
+	public static final ServiceException DB_LIST_IS_NULL = new ServiceException(90090004, "数据库操作,list返回null");
 
 	/**
 	 * Token 验证不通过
 	 */
-	public static final ServiceException TOKEN_IS_ILLICIT = new ServiceException(90040005, "Token 验证非法");
+	public static final ServiceException TOKEN_IS_ILLICIT = new ServiceException(90090005, "Token 验证非法");
 	/**
 	 * 会话超时　获取session时，如果是空，throws 下面这个异常 拦截器会拦截爆会话超时页面
 	 */
-	public static final ServiceException SESSION_IS_OUT_TIME = new ServiceException(90040006, "会话超时");
+	public static final ServiceException SESSION_IS_OUT_TIME = new ServiceException(90090006, "会话超时");
 
 	/**
 	 * 获取序列出错
 	 */
-	public static final ServiceException DB_GET_SEQ_NEXT_VALUE_ERROR = new ServiceException(90040007, "获取序列出错");
+	public static final ServiceException DB_GET_SEQ_NEXT_VALUE_ERROR = new ServiceException(90090007, "获取序列出错");
 
 	
 	protected String errMsg;
