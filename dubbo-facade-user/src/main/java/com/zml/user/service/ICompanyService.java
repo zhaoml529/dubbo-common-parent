35e2778931ffaff39ad64e182db0775e05b2cfb9
zhaoml529 <zhaoml529@163.com>
2017-01-02 22:24:21 +0800

    公司服务接口

--- dubbo-facade-user/src/main/java/com/zml/user/service/ICompanyService.java
+++ dubbo-facade-user/src/main/java/com/zml/user/service/ICompanyService.java
@@ -1,8 +1,15 @@
 package com.zml.user.service;
 
+import java.util.List;
+import java.util.Map;
+
 import com.zml.user.entity.Company;
 
 public interface ICompanyService {
 
 	public Long addCompany(Company company);
+	
+	public Long updateCompany(Company company);
+	
+	public List<Company> findAll(Map<String, Object> map);
 }
