d03eac39a85b28f61f6bc5941eb3a74916509355
zhaoml529 <zhaoml529@163.com>
2017-01-07 17:45:29 +0800

    company实现类

---
+++ dubbo-service-user/src/main/java/com/zml/user/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,30 @@
+package com.zml.user.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.zml.user.entity.Company;
+import com.zml.user.exceptions.CompanyServiceException;
+import com.zml.user.service.ICompanyService;
+
+@Service("companyService")
+public class CompanyServiceImpl implements ICompanyService {
+
+	public Long addCompany(Company company) throws CompanyServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Long updateCompany(Company company) throws CompanyServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Company> findAll(Map<String, Object> map) throws CompanyServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
