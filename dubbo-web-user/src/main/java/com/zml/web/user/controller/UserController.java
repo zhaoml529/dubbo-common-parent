fbc505787b7cf78bdf28eccd56c92e91925f275b
zhaoml529 <zhaoml529@163.com>
2017-01-22 18:21:15 +0800

    UserController

---
+++ dubbo-web-user/src/main/java/com/zml/web/user/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.zml.web.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
