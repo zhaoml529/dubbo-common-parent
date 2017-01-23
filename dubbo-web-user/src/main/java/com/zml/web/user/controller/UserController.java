17b53bc33359812ce675693caca870e9b8d7b095
zhaoml529 <zhaoml529@163.com>
2017-01-23 15:34:00 +0800

    dubbo-web-user

--- dubbo-web-user/src/main/java/com/zml/web/user/controller/UserController.java
+++ dubbo-web-user/src/main/java/com/zml/web/user/controller/UserController.java
@@ -1,10 +1,31 @@
 package com.zml.web.user.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.zml.user.entity.User;
+import com.zml.user.service.IUserService;
+
+@RestController
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private IUserService userService;
 
+	@RequestMapping(value="/user/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> getDetail(@PathVariable("id") long id) {
+		User user = this.userService.getUserById(id);
+		if(user != null) {
+			return new ResponseEntity<User>(user, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+		}
+	}
 }
