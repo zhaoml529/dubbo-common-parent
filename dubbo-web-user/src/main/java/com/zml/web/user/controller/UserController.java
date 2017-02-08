93c784b783b5a15b30ad4f19aea569fb640beb51
zhaoml529 <zhaoml529@163.com>
2017-02-08 15:21:36 +0800

    dubbo-web-user

--- dubbo-web-user/src/main/java/com/zml/web/user/controller/UserController.java
+++ dubbo-web-user/src/main/java/com/zml/web/user/controller/UserController.java
@@ -1,32 +1,34 @@
 package com.zml.web.user.controller;
 
-import javax.annotation.PostConstruct;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.zml.user.entity.User;
 import com.zml.user.service.IUserService;
 
 @RestController
-@RequestMapping("/user")
 public class UserController {
 	
 	@Autowired
 	private IUserService userService;
 	
-	@PostConstruct
-	public void init() {
-		System.out.println("!!!!!!!!!");
-	}
-
-	@RequestMapping(value="/user/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	/**
+	 * 用户详情
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> getDetail(@PathVariable("id") long id) {
 		User user = this.userService.getUserById(id);
 		if(user != null) {
@@ -35,4 +37,81 @@ public class UserController {
 			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
 		}
 	}
+	
+	/**
+	 * 获取所有用户信息
+	 * @return
+	 */
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> listAllUsers() {
+        List<User> users = userService.getAllUser();
+        if(users.isEmpty()){
+            return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT); //You many decide to return HttpStatus.NOT_FOUND
+        }
+        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+    }
+	
+	/**
+	 * 添加用户
+	 * @param user
+	 * @param ucBuilder
+	 * @return
+	 */
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder ucBuilder) {
+		if(this.userService.isUserExist(user)) {
+			System.out.println("A User with name " + user.getUserName() + " already exist");
+            return new ResponseEntity<Void>(HttpStatus.CONFLICT);	// 请求冲突
+		}
+		this.userService.addUser(user);
+		
+		HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/user/{id}").buildAndExpand(user.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	}
+	
+	/**
+	 * 更新用户信息
+	 * @param id
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+        System.out.println("Updating User " + id);
+         
+        User currentUser = this.userService.getUserById(id);
+         
+        if (currentUser == null) {
+            System.out.println("User with id " + id + " not found");
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+ 
+        //currentUser.setUserName(user.getUserName());
+        currentUser.setStaffId(user.getStaffId());
+        currentUser.setPasswd(user.getPasswd());
+        
+        this.userService.updateUser(currentUser);
+        return new ResponseEntity<User>(currentUser, HttpStatus.OK);
+    }
+	
+	/**
+	 * 根据id删除用户
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<User> deleteUser(@PathVariable("id") long id) {
+        System.out.println("Fetching & Deleting User with id " + id);
+ 
+        User user = this.userService.getUserById(id);
+        if (user == null) {
+            System.out.println("Unable to delete. User with id " + id + " not found");
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+ 
+        userService.deleteUser(id);
+        return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+    }
+	
 }
