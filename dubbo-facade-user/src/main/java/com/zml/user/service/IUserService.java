4290d422affab01880f450f126b4e3b620fb1a42
zhaoml529 <zhaoml529@163.com>
2017-02-08 15:23:34 +0800

    IUserService 增加接口方法

--- dubbo-facade-user/src/main/java/com/zml/user/service/IUserService.java
+++ dubbo-facade-user/src/main/java/com/zml/user/service/IUserService.java
@@ -1,5 +1,7 @@
 package com.zml.user.service;
 
+import java.util.List;
+
 import com.zml.user.entity.User;
 import com.zml.user.exceptions.UserServiceException;
 
@@ -12,10 +14,24 @@ public interface IUserService {
 
 	public Long addUser(User user) throws UserServiceException;
 	
+	public List<User> getAllUser() throws UserServiceException;
+	
 	public User getUserByName(String userName) throws UserServiceException;
 	
 	public User getUserByStaffId(String staffId) throws UserServiceException;
 	
 	public User getUserById(Long id) throws UserServiceException;
+	
+	/**
+	 * 通过用户名查询用户是否存在
+	 * @param user
+	 * @return
+	 * @throws UserServiceException
+	 */
+	public boolean isUserExist(User user) throws UserServiceException;
+	
+	public void updateUser(User user) throws UserServiceException;
+	
+	public void deleteUser(Long id) throws UserServiceException;
 
 }
