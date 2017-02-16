b4e5671eb657e2019f2ff2a44670e4937bb3165d
zhaoml529 <zhaoml529@163.com>
2017-02-16 10:16:43 +0800

    添加用户操作日志接口

---
+++ dubbo-facade-user/src/main/java/com/zml/user/service/IUserOperateLogService.java
@@ -0,0 +1,9 @@
+package com.zml.user.service;
+
+import com.zml.user.entity.UserOperateLog;
+import com.zml.user.exceptions.UserOperateServiceException;
+
+public interface IUserOperateLogService {
+
+	public Long addLog(UserOperateLog userOperateLog) throws UserOperateServiceException;
+}
