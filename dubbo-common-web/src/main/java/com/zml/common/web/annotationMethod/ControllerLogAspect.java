5542514230ecae4b47cfa4f9e049c3ec20c61080
zhaoml529 <zhaoml529@163.com>
2017-03-21 23:35:39 +0800

    日志管理AOP

--- dubbo-common-web/src/main/java/com/zml/common/web/annotationMethod/ControllerLogAspect.java
+++ dubbo-common-web/src/main/java/com/zml/common/web/annotationMethod/ControllerLogAspect.java
@@ -36,14 +36,20 @@ public class ControllerLogAspect {
 	@Autowired
 	private RedisUtil<User> userRedis;
 	
-	// Controller层切点 
+	/**
+	 * 配置切入点,该方法无方法体,主要为方便同类中其他方法使用此处配置的切入点
+	 */
 	@Pointcut("@annotation(com.zml.common.annotation.ControllerLog)")
-	public void controllerAspect() {
+	public void aspect() {
 		
 	}
 
-	@Before("controllerAspect()")
-	public  void doBefore(JoinPoint joinPoint) throws Exception {
+	/**
+	 * 配置前置通知,使用在方法aspect()上注册的切入点
+	 * 同时接受JoinPoint切入点对象,可以没有该参数
+	 */
+	@Before("aspect()")
+	public  void before(JoinPoint joinPoint) throws Exception {
 		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();    
 		String userId = (String) request.getAttribute(SystemConstant.CURRENT_USER_ID);
 		if(StringUtils.isNotBlank(userId)) {
